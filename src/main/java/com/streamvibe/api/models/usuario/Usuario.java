f024276bdfe3fdd0743be83210c1213e1d43721d
Francisco <francisco.tonini@aluno.senac.br>
2025-06-16 21:20:32 -0300

    DTO feito para usuario e filme

---
+++ src/main/java/com/streamvibe/api/models/usuario/Usuario.java
@@ -0,0 +1,24 @@
+package com.streamvibe.api.models.usuario;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    private int id;
+    private String nome;           
+    private String dataNascimento; 
+    private String email;          
+    private String senha;          
+    private String cpf;            
+
+    public Usuario(DadosCadastroUsuario dados){
+        this.nome = dados.nome();
+        this.dataNascimento = dados.dataNascimento();
+        this.email = dados.email();
+        this.senha = dados.senha();
+        this.cpf = dados.cpf();
+    }
+}
