f97c47f78b42b1775d91601e0e1bc3125990b263
Francisco <francisco.tonini@aluno.senac.br>
2025-06-16 21:42:03 -0300

    DTO feito para usuario, bairro, filme

--- src/main/java/com/streamvibe/api/models/endereco/DadosCadastroEndereco.java
+++ src/main/java/com/streamvibe/api/models/endereco/DadosCadastroEndereco.java
@@ -1,5 +1,12 @@
 package com.streamvibe.api.models.endereco;
 
-public record DadosCadastroEndereco() {
+public record DadosCadastroEndereco(
+    String rua,        
+    String bairro,     
+    int cep,           
+    String cidade,     
+    String complemento,
+    int numero        
+) {
     
 }
--- src/main/java/com/streamvibe/api/models/endereco/Endereco.java
+++ src/main/java/com/streamvibe/api/models/endereco/Endereco.java
@@ -1,5 +1,27 @@
 package com.streamvibe.api.models.endereco;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Endereco {
-    
+    private String rua;        
+    private String bairro;     
+    private int cep;           
+    private String cidade;     
+    private String complemento;
+    private int numero;        
+
+    public Endereco(DadosCadastroEndereco dados){
+        this.rua = dados.rua();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.cidade = dados.cidade();
+        this.complemento = dados.complemento();
+        this.numero = dados.numero();
+    }
 }
+
