c6e4b881139a4320c334f279560e131dca487cae
Francisco <francisco.tonini@aluno.senac.br>
2025-06-27 21:26:09 -0300

    banco de dados indo pra frente

--- src/main/java/com/streamvibe/api/models/filme/Filme.java
+++ src/main/java/com/streamvibe/api/models/filme/Filme.java
@@ -1,11 +1,7 @@
 package com.streamvibe.api.models.filme;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
